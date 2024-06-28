b9010950ffef799fda7d2ed0ce2f61e38f8b1656
Nicholas Marshall <5720213+nialbima@users.noreply.github.com>
2024-06-28 10:58:28 -0400

    Configure Spec.
    The approach taken here is one I use frequently. I like factorybot + ffaker as a tool that allows for randomized testing as-needed (and static values when it's not). I also added support for defining custom spec functions in a clean, manageable way. I've spent too much time in codebases without clear organizational patterns for macros, and it helps everybody when spec is very clear.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require "rspec/rails"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -20,15 +19,14 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Rails.root.glob("spec/support/**/*.rb").sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  Macros.load_all(config)
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -50,8 +48,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
---
+++ spec/support/capybara.rb
@@ -0,0 +1,15 @@
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/cuprite"
+
+Capybara.register_driver(:custom_cuprite) do |app|
+  Capybara::Cuprite::Driver.new( app, inspector: ENV['INSPECTOR'], window_size: [1200, 800] )
+end
+
+Capybara.default_driver = Capybara.javascript_driver = :custom_cuprite
+
+RSpec.configure do |config|
+  config.prepend_before(:each, type: :system) do
+    driven_by Capybara.javascript_driver
+  end
+end
