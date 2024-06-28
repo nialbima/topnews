469a7b3e77a21a35ed6c24467bac1535e76ed23e
Nicholas Marshall <5720213+nialbima@users.noreply.github.com>
2024-06-28 11:19:08 -0400

    Update binstubs. Run standardrb over codebase. Add a new user to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,23 @@
-User.find_or_initialize_by(email: 'DonaldGMiller@example.com').update({
-  first_name: 'Donald',
-  last_name: 'Miller',
-  password: 'eeMaev2shai'
+User.find_or_initialize_by(email: "me@me.com").update({
+  first_name: "It",
+  last_name: "Me",
+  password: "foobar123"
 })
 
-User.find_or_initialize_by(email: 'LawrenceWGrant@example.com').update({
-  first_name: 'Lawrence',
-  last_name: 'Grant',
-  password: 'ahR7iecai'
+User.find_or_initialize_by(email: "DonaldGMiller@example.com").update({
+  first_name: "Donald",
+  last_name: "Miller",
+  password: "eeMaev2shai"
 })
 
-User.find_or_initialize_by(email: 'MargeRWilliams@example.com').update({
-  first_name: 'Marge',
-  last_name: 'Williams',
-  password: 'Aechugh1ie'
+User.find_or_initialize_by(email: "LawrenceWGrant@example.com").update({
+  first_name: "Lawrence",
+  last_name: "Grant",
+  password: "ahR7iecai"
+})
+
+User.find_or_initialize_by(email: "MargeRWilliams@example.com").update({
+  first_name: "Marge",
+  last_name: "Williams",
+  password: "Aechugh1ie"
 })
